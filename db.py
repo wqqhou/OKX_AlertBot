c8f86afc21c26f8b537da9deff834b1ded63861a
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-12-11 19:49:59 +0800

    update

--- db.py
+++ db.py
@@ -21,8 +21,7 @@ def get_subscribers(syb):
 def add_subscriber(syb, uid):
     list = [get_subscribers(syb)]
     new_list = list.append(uid)
-    cur.execute(f'SELECT * FROM Subscription WHERE syb = "{syb}"')
-    cur.execute(f'UPDATE Subscription SET uid = {new_list} WHERE syb = "{syb}"')
+    cur.execute(f'UPDATE Subscription SET uid = {new_list} WHERE syb = {syb}')
     con.commit()
 
 def check_subscriber(syb, uid):
