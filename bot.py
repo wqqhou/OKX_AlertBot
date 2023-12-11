de0a49839c574181ca3a51ee4bc6926671eb03af
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-12-11 19:21:35 +0800

    update

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ from aiogram.utils import executor
 # Local modules to work with Database and Ton network
 import config
 import info
+import db
 
 # Now all the info about bot work will be printed out to console
 logging.basicConfig(level=logging.INFO)
@@ -22,8 +23,42 @@ bot = Bot(token=config.BOT_TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+@dp.message_handler(commands=['start', 'help'])
+async def welcome_handler(message: types.Message):
+    # Function that sends the welcome message with main keyboard to user
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(KeyboardButton('Register'), KeyboardButton('Unregister'))
 
+    # Send welcome text and include the keyboard
+    await message.answer('Current watch list: TONCOIN',
+                         reply_markup=keyboard,
+                         parse_mode=ParseMode.MARKDOWN)
+    
+@dp.message_handler(commands='Register')
+@dp.message_handler(Text(equals='Register', ignore_case=True))
+async def deposit_handler(message: types.Message):
 
+    uid = message.from_user.id
+    if db.check_subscriber(uid):
+        await message.answer('You are already registered')
+    else:
+        if db.add_subscriber(uid, 'TONCOIN'):
+            await message.answer('You are successfully registered')
+        else:
+            await message.answer('Something went wrong')
+    
+@dp.message_handler(commands='Unregister')
+@dp.message_handler(Text(equals='Unregister', ignore_case=True))
+async def deposit_handler(message: types.Message):
+
+    uid = message.from_user.id
+    if db.check_subscriber(uid):
+        if db.remove_subscriber(uid):
+            await message.answer('You are successfully unregistered')
+        else:
+            await message.answer('Something went wrong')
+    else:
+        await message.answer('You are not registered')
 
 if __name__ == '__main__':
     # Create Aiogram executor for our bot
--- db.py
+++ db.py
@@ -5,7 +5,7 @@ import sqlite3
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
 
-# Create table "Users" with uid and balance rows
+# Create table "Subscription" with uid and balance rows
 cur.execute('''CREATE TABLE IF NOT EXISTS Subscription(
                 syb STRING,
                 uid INTEGER
@@ -16,4 +16,27 @@ def get_subscribers(syb):
     uid_list = cur.fetchone()
     if uid_list:
         return uid_list
+    return False
+
+def add_subscriber(syb, uid):
+    list = get_subscribers(syb)
+    new_list = list.append(uid)
+    cur.execute(f'UPDATE Subscription SET uid = {new_list} WHERE syb = {syb}')
+    con.commit()
+
+def check_subscriber(syb, uid):
+    list = get_subscribers(syb)
+    if list:
+        if uid in list:
+            return True
+    return False
+
+def remove_subscriber(syb, uid):
+    list = get_subscribers(syb)
+    if list:
+        if uid in list:
+            list.remove(uid)
+            cur.execute(f'UPDATE Subscription SET uid = {list} WHERE syb = {syb}')
+            con.commit()
+            return True
     return False
