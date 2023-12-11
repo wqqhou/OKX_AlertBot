8ded42faf52945266f23931581f58b0b98e26eac
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-12-11 20:10:34 +0800

    update

--- db.py
+++ db.py
@@ -14,8 +14,8 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Subscription(
 
 def get_subscribers(syb):
     cur.execute(f'SELECT * FROM Subscription WHERE syb = "{syb}"')
-    print(cur.fetchone()[0])
-    uid_list = json.loads(cur.fetchone()[0])
+    print(cur.fetchone()[1])
+    uid_list = json.loads(cur.fetchone()[1])
     if uid_list:
         return uid_list
     return False
