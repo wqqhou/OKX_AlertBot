f2f3cb0e23757d9f4a693fa75399d1340099965e
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-12-11 22:45:54 +0800

    update

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 # Requests for API, Asyncio to call sleep() in async func
-import time
+import asyncio
 import okx.Account as Account
 
 # Aiogram
@@ -19,7 +19,7 @@ async def start():
 
     while True:
         # 2 Seconds delay between checks
-        time.sleep(60)
+        asyncio.sleep(60)
         resp = accountAPI.get_interest_rate(ccy = "TON")
 
         # Iterating over currencies
