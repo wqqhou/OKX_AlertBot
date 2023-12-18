b1c2e67f0267927610f73c743d44d7ce534ad8a0
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-12-18 18:50:28 +0800

    update

--- info.py
+++ info.py
@@ -1,6 +1,7 @@
 # Requests for API, Asyncio to call sleep() in async func
 import asyncio
 import okx.Account as Account
+import okx.PublicData as Public
 from timeit import default_timer as timer
 
 # Aiogram
@@ -11,6 +12,8 @@ import db
 import config
 
 accountAPI = Account.AccountAPI(config.API_KEY, config.API_SECRET_KEY, config.API_PASSPHRASE, False, flag="0")
+publicAPI = Public.PublicAPI(config.API_KEY, config.API_SECRET_KEY, config.API_PASSPHRASE, False, flag="0")
+
 
 async def start():
 
@@ -20,20 +23,30 @@ async def start():
         # 60 Seconds delay between checks
         start = timer()
         try:
-            resp = accountAPI.get_interest_rate()
+            resp_i = accountAPI.get_interest_rate()
+            resp_f = publicAPI.get_funding_rate()
             uid_list = db.get_subscribers()
         except:
             continue
         alert = False
         msg = '[Rate Alert]'
 
-        # Iterating over currencies
-        for ccy in resp['data']:
+        info: dict = {}
+
+        for inst in resp_f['data']:
+            rate = float(inst['nextFundingRate']) * 100
+            syb = inst['instrumentId'].split('-')[0]
+            info[syb]: list = rate
+
+        for ccy in resp_i['data']:
             rate = float(ccy['interestRate']) * 876000
-            if float(rate) >= 25:
-                syb = ccy['ccy']         
-                msg = msg + f'\n\n{syb}: Current borrowing rate is {rate}%'
+            syb = ccy['ccy']         
+            info[syb].append(rate)
+
+        for syb in info:
+            if info[syb][0] > 0.04 or info[syb][1] > 25:
                 alert = True
+                msg += f'\n\n{syb} Interest Rate: {info[syb][1]} Funding Rate: {info[syb][0]}%'
 
         if alert:        
             for uid in uid_list:
@@ -41,5 +54,7 @@ async def start():
                    await bot.send_message(uid, msg)
                 except:
                     pass
+
         end = timer()
+        
         await asyncio.sleep(300 - (end - start))
